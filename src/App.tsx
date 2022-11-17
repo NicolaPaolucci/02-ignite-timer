25d23b49414426f46ac00ed06c000426d44bd6e0
Nicola Paolucci <nicolamp@live.com>
2022-11-17 15:53:09 -0300

    commit conclusao ignite-timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { ThemeProvider } from 'styled-components'
-
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
+
 import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
